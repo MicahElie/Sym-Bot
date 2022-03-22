fcfed23a1ccaf81b13124de229303b57e4c9e35b
Chromlox <48286062+Chromlox@users.noreply.github.com>
2022-03-22 15:21:57 -0400

    Suppression de la partie TEST

--- Glove/src/main.cpp
+++ Glove/src/main.cpp
@@ -1,65 +1,8 @@
-// #define TEST
-
 #include <Arduino.h>
 #include <SparkFun_ADS1015_Arduino_Library.h>
 #include <MPU9250.h>
 #include <Wire.h>
 #include "BluetoothSerial.h"
-
-#ifdef TEST
-#include <BitBang_I2C.h>
-
-#define SDA_PIN 6
-#define SCL_PIN 7
-
-BBI2C bbi2c;
-const char *szNames[]  = {"Unknown","SSD1306","SH1106","VL53L0X","BMP180", "BMP280","BME280",
-                "MPU-60x0", "MPU-9250", "MCP9808","LSM6DS3", "ADXL345", "ADS1115","MAX44009",
-                "MAG3110", "CCS811", "HTS221", "LPS25H", "LSM9DS1","LM8330", "DS3231", "LIS3DH",
-                "LIS3DSH","INA219","SHT3X","HDC1080","MPU6886","BME680", "AXP202", "AXP192", "24AA02XEXX", 
-                "DS1307", "MPU688X", "FT6236G", "FT6336G", "FT6336U", "FT6436", "BM8563","BNO055"};
-
-void setupI2Cbitbang(){
-  memset(&bbi2c, 0, sizeof(bbi2c));
-  bbi2c.bWire = 0; // use bit bang, not wire library
-  bbi2c.iSDA = SDA_PIN;
-  bbi2c.iSCL = SCL_PIN;
-  I2CInit(&bbi2c, 100000L);
-  delay(100); // allow devices to power up
-}
-
-void bitbang_I2C_Scan(){
-  uint8_t map[16];
-  uint8_t i;
-  int iDevice, iCount;
-  Serial.println("Starting I2C Scan");
-  I2CScan(&bbi2c, map); // get bitmap of connected I2C devices
-  if (map[0] == 0xfe) // something is wrong with the I2C bus
-  {
-    Serial.println("I2C pins are not correct or the bus is being pulled low by a bad device; unable to run scan");
-  }
-  else
-  {
-    iCount = 0;
-    for (i=1; i<128; i++) // skip address 0 (general call address) since more than 1 device can respond
-    {
-      if (map[i>>3] & (1 << (i & 7))) // device found
-      {
-        iCount++;
-        Serial.print("Device found at 0x");
-        Serial.print(i, HEX);
-        iDevice = I2CDiscoverDevice(&bbi2c, i);
-        Serial.print(", type = ");
-        Serial.println(szNames[iDevice]); // show the device name as a string
-        Serial.println(iDevice); // show the device name as the enum index
-      }
-    } // for i
-    Serial.print(iCount, DEC);
-    Serial.println(" device(s) found");
-  }
-  delay(5000);
-}
-#endif
 
 BluetoothSerial SerialBT;
 
