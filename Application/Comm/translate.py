e5c06405f406ef5b19f0e095627f20f752e1baa0
MicahElie <72100729+MicahElie@users.noreply.github.com>
2022-03-22 21:15:47 -0400

    Fonction translate ( #57 )

---
+++ Application/Comm/translate.py
@@ -0,0 +1,94 @@
+from enum import Enum
+from Comm import ControlMessage
+from Comm import MessageIO
+
+class translate:
+
+    JOG = 1;
+    JOINT = 2;
+    CART = 3;
+    AI = 4;
+    INTERFACE = 5;
+
+    # Forme du message recu par le gant
+    # mode;index;majeur;annulaire;auriculaire;Y;Z;rotation;
+
+
+    def __init__(self, length, msgIO):
+        self.msgGlove = [length]
+        self.msgIO = msgIO
+
+
+    def chooseMode(self,msgGlove):
+        '''This function analyses the message from the glove, determines the mode and calls the right translation function
+
+        Parameters
+        -------------
+        msgGlove : double[] Data from the glove
+        '''
+        self.msg = msgGlove.split(";")
+        symbotMode = self.msg[0]
+
+        match symbotMode:
+            case 1:
+                print("this is jog mode")
+                self.jogMode(self.msg)
+                
+            case 2:
+                print("this is joint mode")
+            case 3:
+                print("this is cartesian mode")
+            case 4:
+                print("this is AI mode")
+            case 5:
+                print("this is interface mode")
+
+    def jogMode(self, msg, flexion, rev):
+        '''This function moves the motors as long as fingers are  in flexion
+        
+        Parameters
+        --------------
+        msg : double[] Message from the glove
+        flexion : double Value at which a finger is considered in flexion
+        rev : double Value at which the hand is considered up-side-down
+        '''
+        flex = flexion
+        reverse = rev
+        tabMsg = [3]*0
+        for id in range(0,3):
+            if msg[id + 1] >= flex:
+                tabMsg[id] = msg[id + 1]
+            elif msg[id + 1] < flex:
+                tabMsg[id] = msg[id + 1]
+            else:
+                print("Values from the glove not precise enough")
+        
+        if msg[8] >= reverse:
+            for id in tabMsg:
+                tabMsg[id] = tabMsg[id]*-1
+        
+
+        msgMotor = ControlMessage(ControlMessage.SET_JOG, tabMsg)
+        self.msgIO.sendMessage(0, msgMotor)
+
+    def jointMode(self, msg):
+
+        msgMotor = ControlMessage(ControlMessage.SET_JOIN_POSITION, tabMsg)
+        self.msgIO.sendMessage(0, msgMotor)
+    
+    def cartMode(self,msg):
+        print("This is meant to be something")
+    
+    def aiMode (self, msg):
+        print("This is meant to be something")
+    
+    def interMode (self, msg):
+
+
+
+
+
+
+
+
+    
