07a540a0f8643dfa2fe8b4549918bcabf894da46
MicahElie <72100729+MicahElie@users.noreply.github.com>
2022-03-27 17:01:43 -0400

    test de jogMode fonctionnel
    Manque test pour répétition de commande
    
    Co-Authored-By: Chromlox <48286062+Chromlox@users.noreply.github.com>

--- Application/Comm/translate.py
+++ Application/Comm/translate.py
@@ -4,43 +4,69 @@ from Comm import MessageIO
 
 class translate:
 
-    JOG = 1;
-    JOINT = 2;
-    CART = 3;
-    AI = 4;
-    INTERFACE = 5;
+    JOG = 0;
+    JOINT = 1;
+    CART = 2;
+    AI = 3;
+    INTERFACE = 4;
 
-    lastMsg = [0,0,0,0]
+    lastMsgMotor = [0,0,0,0]
+    currentMsgMotor = [0,0,0,0]
     enumRep = 0
+
+    mode = 0
+    flexion = [0,0,0,0]
+    imu = [0,0,0]
 
     # Forme du message recu par le gant
     # "Mode: ", "Flex : [4]", "IMU : [3]"
 
     def __init__(self, msgIO):
         self.msgIO = msgIO
+        
+    def getMode(self):
+        return self.mode
+
+    def getFlex(self, index):
+        return self.flexion[index]
+
+    def getImu(self, index):
+        return self.imu[index]
+    
+    def getMsgMotor(self,index):
+        return self.currentMsgMotor[index]
 
 
     def chooseMode(self,msgGlove):
         '''This function analyses the message from the glove, determines the mode and calls the right translation function
 
-        Parameters
+        Parameters,
         -------------
         msgGlove : double[] Data from the glove
         '''
 
-        match msgGlove["Mode"]:
+        match msgGlove['Mode']:
             case 1:
                 print("this is jog mode")
+                self.mode = msgGlove['Mode']
                 self.jogMode(msgGlove["Flex"], msgGlove["IMU"])
                 
             case 2:
                 print("this is joint mode")
+                self.mode = msgGlove['Mode']
+
             case 3:
                 print("this is cartesian mode")
+                self.mode = msgGlove['Mode']
+
             case 4:
                 print("this is AI mode")
+                self.mode = msgGlove['Mode']
+
             case 5:
                 print("this is interface mode")
+                self.mode = msgGlove['Mode']
+
 
     def jogMode(self, flexion, imu):
         '''This function moves the motors as long as fingers are  in flexion
@@ -53,7 +79,7 @@ class translate:
         tresholdFlex = 80
         treshold_IMU_pos = 0.50
         treshold_IMU_neg = -0.50 
-        msgMotor = [0,0,0,0]
+        self.currentMsgMotor = [0,0,0,0]
         sumFinger = 0
         griperIncrement = 0
 
@@ -62,7 +88,7 @@ class translate:
             # Verify the flexion of the 3 fingers associated to joint 1,2,3
             for id in range(0,3):
                 if flexion[id] >= tresholdFlex:
-                    msgMotor[id] = -1
+                    self.currentMsgMotor[id] = -1
                     sumFinger += 1
 
             # Verify the flexion of the last finger associated with the gripper        
@@ -71,62 +97,64 @@ class translate:
                     griperIncrement = 1
                 elif griperIncrement == 1:
                     griperIncrement = 0
-                msgMotor[3] = griperIncrement
+                self.currentMsgMotor[3] = griperIncrement
 
         elif imu[2] >= treshold_IMU_pos:  
             # Verify the flexion of the 3 fingers associated to joint 1,2,3
             for id in range(0,3):
                 if flexion[id] >= tresholdFlex:
-                    msgMotor[id] = 1
+                    self.currentMsgMotor[id] = 1
                     sumFinger += 1
 
             # Verify the flexion of the last finger associated with the gripper        
             if flexion[3] >= tresholdFlex and sumFinger == 3:
+                # griperIncrement = !griperIncrement
                 if griperIncrement  == 0:
                     griperIncrement = 1
                 elif griperIncrement == 1:
                     griperIncrement = 0
-                msgMotor[3] = griperIncrement
+                self.currentMsgMotor[3] = griperIncrement
 
         else: pass
 
-        if msgMotor == self.lastMsg and self.enumRep < 3:
-            msg_to_motor = ControlMessage(ControlMessage.SET_JOG, self.lastMsg)
+        if self.currentMsgMotor == self.lastMsgMotor and self.enumRep < 3:
+            msg_to_motor = ControlMessage(ControlMessage.SET_JOG, self.lastMsgMotor)
             self.msgIO.sendMessage(0, msg_to_motor)
             self.enumRep += 1
-        elif msgMotor == self.lastMsg and self.enumRep >= 3:
-            msg_to_motor = ControlMessage(ControlMessage.SET_JOG, msgMotor)
+        elif self.currentMsgMotor == self.lastMsgMotor and self.enumRep >= 3:
+            msg_to_motor = ControlMessage(ControlMessage.SET_JOG, self.currentMsgMotor)
             self.msgIO.sendMessage(0, msg_to_motor)
-            self.lastMsg = msgMotor
+            self.lastMsgMotor = self.currentMsgMotor
             self.enumRep = 0
-        elif msgMotor != self.lastMsg and self.enumRep < 3:
+        elif self.currentMsgMotor != self.lastMsgMotor and self.enumRep < 3:
             print("!=")
             self.enumRep = 0
-            self.lastMsg = msgMotor
-
-    def cartMode(self,msg):
-        '''This function moves to motors with using precise position in space'''
-        print("This is meant to be something")
+            self.lastMsgMotor = self.currentMsgMotor
 
     def aiMode (self, msg):
         print("This is meant to be something")
+    
+    def cartMode(self,msg):
+        '''This function moves to motors with using precise position in space'''
+        print("This is meant to be something")
+        
 
     # def jointMode(self, msg, rev):
     #     reverse = rev
-    #     msgMotor = [3]*0
+    #     currentMsgMotor = [3]*0
 
     #     for id in range(0,4):
-    #         msgMotor[id] = self.__glovetomotor(msg[id+1])
+    #         currentMsgMotor[id] = self.__glovetomotor(msg[id+1])
     #     print("This is meant to be something")
-    #     # msgMotor = ControlMessage(ControlMessage.SET_JOIN_POSITION, msgMotor)
-    #     self.msgIO.sendMessage(0, msgMotor)
+    #     # currentMsgMotor = ControlMessage(ControlMessage.SET_JOIN_POSITION, currentMsgMotor)
+    #     self.msgIO.sendMessage(0, currentMsgMotor)
     
     # def interMode (self, msg):
     #     return
 
-    def __glovetomotor(self, gloveValue):
-        angleValue = gloveValue*90/100
-        return angleValue
+    # def __glovetomotor(self, gloveValue):
+    #     angleValue = gloveValue*90/100
+    #     return angleValue
 
 
 
