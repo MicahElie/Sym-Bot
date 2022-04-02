05e92144731333d2c464f96296d398432ffc0788
Chromlox <48286062+Chromlox@users.noreply.github.com>
2022-04-02 19:35:11 -0400

    - Ajout d'une structure Hand qui regroupe toutes les données des capteurs
    - Ajout d'un protocole de vérification d'erreurs avec DEL

--- Glove/src/main.cpp
+++ Glove/src/main.cpp
@@ -4,29 +4,46 @@
 #include <Wire.h>
 #include "BluetoothSerial.h"
 
+//Structs:
+struct Color{
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+};
+struct Hand{
+  static const uint8_t FINGERS = 4;
+  static const uint8_t AXIS = 3;
+
+  uint16_t max[FINGERS] = {0, 0, 0, 0};
+  uint16_t fingers[FINGERS] = {0, 0, 0, 0}; // Order for the right hand: {INDEX MIDDLE RING PINKY}
+  uint16_t offsets[FINGERS] = {0,0,0,0};//last offset 190??
+  float imu[AXIS] = {0, 0, 0}; // Order for data: {LINEAR_Y LINEAR_Z ROTATION_Z}
+} hand = {};
+
+// Const:
+const unsigned MILLISECONDS = 10;
+const uint8_t MODE_MAX = 3;
+const Color RED = {255,0,0}; // FATAL ERROR
+const Color ORANGE = {255,115,0}; // JOG MODE
+const Color GREEN = {70,255,0}; // CARTESIAN MODE
+const Color BLUE = {0,240,255}; // AI MODE
 // Pins:
 const uint8_t LED = 2;
 const uint8_t BUTTON = 23;
 
-// Const:
-const unsigned MILLISECONDS = 10;
-
 // Mode:
 uint8_t mode = 0;
-uint8_t MODE_MAX = 4;
+Color colorsModes[MODE_MAX] = {ORANGE, GREEN, BLUE};
 // Flex Sensors:
 ADS1015 pinkySensor;
 ADS1015 indexSensor;
-uint16_t hand_max[4] = {0, 0, 0, 0};
-uint16_t hand[4] = {0, 0, 0, 0}; // Order for the right hand: {INDEX MIDDLE RING PINKY}
-const uint16_t offset[4] = {0,0,0,0};//190
 // IMU:
 MPU9250 mpu;
-float imu[4] = {0, 0, 0}; // Order for data: {LINEAR_Y LINEAR_Z ROTATION_Z}
-
 // Communication:
 BluetoothSerial SerialBT;
 String serialData;
+// Flags:
+bool isFatalError[2] = {false, false};
 
 /*-------------------------------------------------- setup fonctions --------------------------------------------------*/
 
@@ -37,25 +54,29 @@ void setupBluetooth() {
 void setupFlexSensors(){
   //Set up each sensor, change the addresses based on the location of each sensor
   if (pinkySensor.begin(ADS1015_ADDRESS_SDA) == false) {
-     Serial.println("Pinky not found. Check wiring.");
-     while (1);
+     SerialBT.println("Pinky not found. Check wiring.");
+     isFatalError[0] = true;
   }
   if (indexSensor.begin(ADS1015_ADDRESS_GND) == false) {
-     Serial.println("Index not found. Check wiring.");
-     while (1);
+     SerialBT.println("Index not found. Check wiring.");
+     isFatalError[0] = true;
   }  
   pinkySensor.setGain(ADS1015_CONFIG_PGA_TWOTHIRDS); // Gain of 2/3 to works well with flex glove board voltage swings (default is gain of 2)
   indexSensor.setGain(ADS1015_CONFIG_PGA_TWOTHIRDS); // Gain of 2/3 to works well with flex glove board voltage swings (default is gain of 2)
   
   for (int finger = 0; finger < 2; finger++) {
-    hand_max[finger] = indexSensor.getAnalogData(!finger);
-    hand_max[finger + 2] = pinkySensor.getAnalogData(!finger);
+    hand.max[finger] = indexSensor.getAnalogData(!finger);
+    hand.max[finger + 2] = pinkySensor.getAnalogData(!finger);
   }
 }
 
 void setupIMUSensor(){
-  mpu.setup(0x68);
-  mpu.calibrateAccelGyro();
+  isFatalError[1] = !mpu.setup(0x68);
+  if (isFatalError[1]){
+    SerialBT.println("MPU9250 not found. Check wiring.");
+  } else {
+    mpu.calibrateAccelGyro();
+  }
 }
 
 void setupLED(){
@@ -83,8 +104,8 @@ void buttonRead(){
     // if the button state has changed:
     if (reading != buttonState) {
       buttonState = reading;
-      // only toggle the LED if the new button state is HIGH
-      if (buttonState == HIGH) {
+      // only toggle the LED if the new button state is HIGH(PULLDOWN) / LOW(PULLUP)
+      if (buttonState == LOW) {
         mode = (mode+1) % MODE_MAX;
       }
     }
@@ -101,19 +122,19 @@ void setupButton(){
 
 void flexSensorsData(){
   for (int finger = 0; finger < 4; finger++){
-    hand[finger] = 0;
+    hand.fingers[finger] = 0;
   }
   for (int finger = 0; finger < 2; finger++) {
-    if (indexSensor.getAnalogData(!finger) < hand_max[finger]){
-      hand[finger] = hand_max[finger] - indexSensor.getAnalogData(!finger) - offset[finger];
-      if (hand[finger] > 100){
-        hand[finger] = 0;
+    if (indexSensor.getAnalogData(!finger) < hand.max[finger]){
+      hand.fingers[finger] = hand.max[finger] - indexSensor.getAnalogData(!finger) - hand.offsets[finger];
+      if (hand.fingers[finger] > 100){
+        hand.fingers[finger] = 0;
       }
     }
-    if (pinkySensor.getAnalogData(!finger) < hand_max[finger + 2]){
-      hand[finger + 2] = hand_max[finger + 2] - pinkySensor.getAnalogData(!finger) - offset[finger + 2];
-      if (hand[finger + 2] > 100){
-        hand[finger + 2] = 0;
+    if (pinkySensor.getAnalogData(!finger) < hand.max[finger + 2]){
+      hand.fingers[finger + 2] = hand.max[finger + 2] - pinkySensor.getAnalogData(!finger) - hand.offsets[finger + 2];
+      if (hand.fingers[finger + 2] > 100){
+        hand.fingers[finger + 2] = 0;
       }
     }
   }
@@ -130,19 +151,19 @@ void IMUSensorData(){
 
     // imu[0] += mpu.getLinearAccY()*9.81f*(milliseconds/1000.0); //Distance (Verifier l'axe X et Z pour voir si la main est bien plat => Z = 1.03 et X = 0.02 ou mettre un intervalle)
     if ((mpu.getAccY() >= 0.05f || mpu.getAccY() <= -0.05f) && mpu.getAccZ() > 0.85f){
-      imu[0] += 0;
+      hand.imu[0] += 0;
     }
     else {
-      imu[0] += mpu.getLinearAccY()*9.81f*(MILLISECONDS/1000.0);
+      hand.imu[0] += mpu.getLinearAccY()*9.81f*(MILLISECONDS/1000.0);
     }
       
-    imu[1] = mpu.getLinearAccZ()*9.81f; //Height (Verifier l'axe X et Z pour voir si la main est bien plat => Z = 1.03 et X = 0.02 ou mettre un intervalle)
+    hand.imu[1] = mpu.getLinearAccZ()*9.81f; //Height (Verifier l'axe X et Z pour voir si la main est bien plat => Z = 1.03 et X = 0.02 ou mettre un intervalle)
     if ((mpu.getAccY() >= 0.05f || mpu.getAccY() <= -0.05f) && mpu.getAccZ() > 0.85f){
-      imu[1] = 0;
+      hand.imu[1] = 0;
     }
-    imu[2] = mpu.getAccZ(); //Rotation
-    if (imu[2] <= 0.15f && imu[2] >= -0.15f){
-      imu[2] = 0;
+    hand.imu[2] = mpu.getAccZ(); //Rotation
+    if (hand.imu[2] <= 0.15f && hand.imu[2] >= -0.15f){
+      hand.imu[2] = 0;
     }
     // imu[3] = mpu.getAccY(); //Rotation
     // if (imu[3] <= 0.01f && imu[3] >= -0.01f){
@@ -152,7 +173,7 @@ void IMUSensorData(){
 }
 
 void activationLED(){
-  if (imu[2] == 0.0f){
+  if (hand.imu[2] == 0.0f){
     digitalWrite(LED, HIGH);
   }
   else {
@@ -166,7 +187,7 @@ void stringToSend(){
   for (int finger = 0; finger < 4; finger++){
   //   //serialData += finger + ":";
   //   // serialData += hand[finger] + ";";
-    serialData += String(hand[finger]) + ";";
+    serialData += String(hand.fingers[finger]) + ";";
   }
 
   for (int i = 0; i < 3; i++){
@@ -181,30 +202,39 @@ void stringToSend(){
     //     serialData += "R:";
     //     break;
     // }
-    serialData += String(imu[i]) + ";";
+    serialData += String(hand.imu[i]) + ";";
   }
 }
 
 /*-------------------------------------------------- main fonctions --------------------------------------------------*/
 void setup() {
+  //Communication setup:
   Wire.begin();
   Serial.begin(115200);
   setupBluetooth();
   delay(1000);
-
+  //Sensors and UI setup:
   setupLED();
   setupButton();
   setupIMUSensor();
   setupFlexSensors();
+  // Error flag raised verification:
+  while (isFatalError[0] || isFatalError[1]){
+    // red led of death:
+  }
 }
 
 void loop() {
+  // Gathers data:
   flexSensorsData();
   IMUSensorData();
+  // UI States:
   activationLED();
   buttonRead();
+  //Send data:
   stringToSend();
   Serial.println(serialData);
   SerialBT.println(serialData);
+
   delay(MILLISECONDS);
 }
