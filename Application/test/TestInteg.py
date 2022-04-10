885984dafa6954d91763c8bca6fcec38a463cd88
MicahElie <72100729+MicahElie@users.noreply.github.com>
2022-04-10 14:44:11 -0400

    Ménage
    Suppression fichier cinématique
    déplacement vectorBase dans Util
    Réaménagement des test
    
    Co-Authored-By: JuThe72 <97997557+JuThe72@users.noreply.github.com>

--- Application/test/TestInteg.py
+++ Application/test/TestInteg.py
@@ -3,29 +3,15 @@ from Comm.SerialComm import *
 from Comm.driveManager import *
 
 
-#from Cinematic.PositionSolver import *
-from Cinematic.JoinSystem import *
-#from Cinematic.LinearJoin import *
-from Cinematic.RevoluteJoin import *
-from Cinematic.robotAPI import *
-
 import time
 
 class MotorTesting():
 
     def __init__ (self):
     
-        messageIO = MessageIO()
-        messageIO.addDevice(SerialComm("COM3", 57600))
-        driveManager = DriveManager([0, 0, 0], messageIO)
+        msgIO = MessageIO()
+        msgIO.addDevice(SerialComm("COM3", 57600))
 
-        #Gestion des joints    
-        JS = JoinSystem([RevoluteJoin(VectorSpaceAxis.Y, np.array([0.0, 0.0, 0.0]), [-math.pi/2, math.pi/2], hardwareStepDistance= math.pi*2/4096)])
-        JS.addJoin(RevoluteJoin(VectorSpaceAxis.X, np.array([0, 0.245, 0]), [-math.pi/4, math.pi/4], hardwareStepDistance= math.pi*2/4096))
-        JS.addJoin(RevoluteJoin(VectorSpaceAxis.X, np.array([0, 0.245, 0]), [-math.pi/4, math.pi/4], hardwareStepDistance= math.pi*2/4096))
-
-        self.robot = robotAPI(JS,[0., 0., 0.], driveManager)
-        print('Debut du test')
 
     def Motor_position_solo (self, dep = 0):
         '''Déplacement de chacun des moteur en solo'''
@@ -33,7 +19,7 @@ class MotorTesting():
         print('Retour au point de départ')
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0, 0, 0])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
 
@@ -41,9 +27,9 @@ class MotorTesting():
         print('Déplacement du moteur de base')
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [dep, 0, 0])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0,0,0])
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
 
@@ -51,9 +37,9 @@ class MotorTesting():
         print("Déplacement du moteur de l'épaule")
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0, dep, 0])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0,0,0])
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
 
@@ -61,9 +47,9 @@ class MotorTesting():
         print('Déplacement du moteur du coude')
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0, 0, dep])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0,0,0])
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
 
@@ -78,7 +64,7 @@ class MotorTesting():
         print('Retour au point de départ')
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0, 0, 0])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
 
@@ -86,9 +72,9 @@ class MotorTesting():
         print('Les moteur vont tous à la même position')
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [dep, dep, dep])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0,0,0])
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
 
@@ -96,9 +82,9 @@ class MotorTesting():
         print('Les moteurs vont tous à des positions différente ')
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [dep, dep+100, dep+200])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0,0,0])
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
     
@@ -113,7 +99,7 @@ class MotorTesting():
         print('Retour au point de départ')
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0, 0, 0])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
 
@@ -121,14 +107,14 @@ class MotorTesting():
         print('Homing identique pour chaque moteur')
         msg = ControlMessage(ControlMessage.SET_HOME, [home, home, home])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(1)
         
         #Les moteurs vont au nouveau home
         msg = ControlMessage(ControlMessage.GOTO_HOME)
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
 
@@ -136,7 +122,7 @@ class MotorTesting():
         print('Retour au point de départ')
         msg = ControlMessage(ControlMessage.SET_JOIN_POSITION, [0, 0, 0])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
 
@@ -145,14 +131,14 @@ class MotorTesting():
 
         msg = ControlMessage(ControlMessage.SET_HOME, [home, home + 100, home + 200])
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(1)
         
         #Les moteurs vont au nouveau home
         msg = ControlMessage(ControlMessage.GOTO_HOME)
         print(msg)
-        self.robot.executeCommand(msg)
+        self.msgIO.sendMessage(0, msg)
 
         time.sleep(5)
 
