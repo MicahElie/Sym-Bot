3c79ea3cb2126732b59ec6796497b9c7fa56ef81
JuThe72 <97997557+JuThe72@users.noreply.github.com>
2022-04-16 09:32:01 -0400

    #50 Add comment BluetoothComm.py

--- Application/Comm/BluetoothComm.py
+++ Application/Comm/BluetoothComm.py
@@ -1,17 +1,19 @@
-from wsgiref import validate
+# https://github.com/pybluez/pybluez
 import bluetooth
 # https://docs.python.org/3/library/socket.html#socket.socket.listen
-import socket
+# import socket     # Wireless Communication - Low Level Alternative  
 
-# MAC = '58:BF:25:37:A6:9A'
-# uuid = "00001101-0000-1000-8000-00805f9b34fb"
+# Exemple ESP32 Specifications
+# MAC   = '58:BF:25:37:A6:9A'
+# uuid  = "00001101-0000-1000-8000-00805f9b34fb"
 
 class Bluetooth:
-    '''This class represents the bluetooth communication with a device; in this case, it's an ESP32
+    '''This class represents the bluetooth communication with a device;
+        in this case, it's an ESP32
 
     Attributes:
     -------------
-    This class doesn't have any attributes
+    validateMsg :   boolean      indicates received message is in the good shape
     '''
     validateMsg = False
 
@@ -20,13 +22,14 @@ class Bluetooth:
 
         Parameters:
         -------------
-        mac :   string  Adress for connection
+        mac  :  string  Network Address for connection
         port :  int     That's the communication port
         uuid :  string  Give the ID of what we want to communicate with     
         '''
         self.port = port
         self.host = mac
         
+        # Collect the device informations
         services = bluetooth.find_service(uuid=uuid, address=mac)
         first_match = services[0]
         self.port = first_match["port"]
@@ -37,7 +40,10 @@ class Bluetooth:
         # Create the client socket
         self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
         self.sock.connect((self.host, self.port))
+
         '''
+        # Alternative
+        # Create the client socket (Low Level)
         self.sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
         self.sock.connect((mac, self.port))
         '''
@@ -51,11 +57,14 @@ class Bluetooth:
         '''
         self.buffer = []
 
-        data = self.sock.recv(64)
+        data = self.sock.recv(64)           # Get Next 64 bytes
         self.buffer = data.decode()
 
+        # Short check if the message is in the minimal good format
+        # Expected message : "0|##0|##0|##0|##0|##0.00|##0.00|##0.00|\r\n"
+        # len() > 16 : To eliminate the incomplete message
+        # [-2:] == "\r\n" : Terminate by Enter
         if(len(self.buffer) > 16 and self.buffer[-2:] == "\r\n"):
-        # if(self.buffer[-2:] == "\r\n"):
             return True
         else:
             return False
@@ -69,6 +78,8 @@ class Bluetooth:
         '''
         self.validateMsg = False
 
+        # Formatting in dictionary to translate.py module
+        # try is to catch wrong data
         text = (self.buffer[:-3]).split("|")
         try:
             msg = {}
@@ -83,7 +94,19 @@ class Bluetooth:
         return msg
 
     def isValidMsg(self):
-        return self.validateMsg
+        '''Function return the validateMsg value
+        
+        Parameters:
+        -----------------
+        This function has no parameters
+        '''
+       return self.validateMsg
 
     def closeConnection(self):
-        self.sock.close()
+        '''Function close the socket connection
+        
+        Parameters:
+        -----------------
+        This function has no parameters
+        '''
+       self.sock.close()
