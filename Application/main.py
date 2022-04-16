f46410714af7924276c7bdf1eaf6697e775a8062
JuThe72 <97997557+JuThe72@users.noreply.github.com>
2022-04-16 10:19:01 -0400

    Update comment in main.py & rename Micah to SymBot

--- Application/main.py
+++ Application/main.py
@@ -10,33 +10,32 @@ if __name__ == '__main__':
     # Select Communicatino's type between "USB" (Simuling UI) or "BT" (Glove)
     TypeComm = "BT" 
 
-    commPort = None
-    messageIO = MessageIO()
-    Micah = translate(messageIO)
-    messageIO.addDevice(SerialComm("/dev/ttyACM0", 57600))  #Port vers open
+    messageIO = MessageIO()                                 # Create Object to charge the communication
+    SymBot = translate(messageIO)                           # Create Object to charge the control
+    messageIO.addDevice(SerialComm("/dev/ttyACM0", 57600))  # Robotic Arm (OpenCr Board)
 
     if TypeComm == "BT":
-        ''' Commmunicatino Bluetooth '''
-        MAC_ = '58:BF:25:37:A6:9A'
-        uuid_ = "00001101-0000-1000-8000-00805f9b34fb"
+        ''' Commmunication Bluetooth '''
+        MAC_ = '58:BF:25:37:A6:9A'                          # your MAC BT device
+        uuid_ = "00001101-0000-1000-8000-00805f9b34fb"      # your uuid BT device
         bt = Bluetooth(MAC_,1,uuid_)
         while True:
             if bt.isMessageAvailable():
                 msg = bt.readMessage()
                 if bt.isValidMsg():
-                    # print(msg)
-                    Micah.chooseMode(msg)
+                    print(msg)                      # View message flow and detect any problem (like latency)
+                    SymBot.chooseMode(msg)          # Process the message
 
     elif TypeComm == "USB":
-        ''' Communicatino USB '''
-        Serial1 = SerialComm("/dev/ttyACM1", 57600)
+        ''' Communication USB '''
+        Serial1 = SerialComm("/dev/ttyACM1", 57600) # Alternative for BT Communication
 
         while True:
             while Serial1.isMessageAvailable():
                 msg = Serial1.readMessage()
                 if Serial1.isGoodMsg(msg):
                     msg = Serial1.getDigital()
-                    print(msg)
-                    Micah.chooseMode(msg)
+                    print(msg)                      # View message flow and detect any problem (like latency)
+                    SymBot.chooseMode(msg)          # Process the message
     else:
         print(TypeComm + " not supported")
